b6aa98915a0da4fdecce7c0515bb590f26fa5d94
Hamza <p229222@pwr.nu.edu.pk>
2025-09-09 14:38:13 +0500

    stage3

--- check.py
+++ check.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 data = {
-    'id':[1,2,3,4,5],
-    'name':['Hamza','mustafa','ashir','aimal','tayab']
+    'id':[1,2,3,4,5,6],
+    'name':['Hamza','mustafa','ashir','aimal','tayab','amir']
 }
 
 df = pd.DataFrame(data)
